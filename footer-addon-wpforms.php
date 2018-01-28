d1509eb7213532d9a350db163bce206572d7204b
Bill Erickson <bill.erickson@gmail.com>
2018-01-27 20:53:30 -0600

    update textdomain, fixes #1

--- footer-addon-wpforms.php
+++ footer-addon-wpforms.php
@@ -6,7 +6,7 @@
  * Version:     1.0.0
  * Author:      Bill Erickson
  * Author URI:  https://www.billerickson.net
- * Text Domain: integrate-convertkit-wpforms
+ * Text Domain: form-footer-addon-wpforms
  * Domain Path: /languages
  * License:     GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
